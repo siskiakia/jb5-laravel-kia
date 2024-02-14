dbdd58ab09b08c36d04165f6d7ac1a34c73d5689
siskiakia <siskiapplg@gmail.com>
2024-02-14 08:48:10 +0700

    first commit

---
+++ perpus/resources/views/footer.blade.php
@@ -0,0 +1,27 @@
+<p>
+    <!--awal FOOTER-->
+    <div class="col-md-12">
+        <div class="row" style="background:rgba(63, 67, 102, 0.562)">
+            <div class="col-md-9">
+                <h3>Visi</h3>
+                <p>Menjadi pusat pembelajaran yang inspiratif dan inklusif bagi seluruh siswa SMKN 2 Bangkalan untuk meningkatkan literasi, kreativitas, dan pengetahuan dalam mendukung pengembangan diri dan prestasi akademik
+
+                <p>
+                <h3>Misi</h3>
+                <ul>
+                    <li>Mendorong budaya literasi dan minat membaca melalui program-program edukasi, promosi literasi, dan kegiatan-kegiatan budaya yang menarik</li>
+                    <li>Menjadi pusat kolaborasi dan pertukaran ide antara siswa, guru, dan komunitas untuk memperluas wawasan, pemahaman, dan keterampilan dalam berbagai bidang pengetahuan</li>
+                    <li>Melakukan evaluasi terus-menerus terhadap layanan dan program perpustakaan untuk meningkatkan kualitas dan relevansi dalam mendukung pencapaian tujuan pendidikan SMKN 2 Bangkalan</li>
+                </ul>                     
+            </div>
+            <div class="col-md-3">
+                Copyright &copy; 2024. All right reserve
+                <p>
+                by Siskiatun Nafisah</div>
+        </div>
+    </div>
+    <!--akhir FOOTER-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+       
---
+++ perpus/resources/views/view_home.blade.php
@@ -0,0 +1,22 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Deskripsi perpustakaan</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+        Perpustakaan SMKN 2 Bangkalan merupakan fasilitas pendidikan yang menyediakan akses dan layanan informasi untuk mendukung proses pembelajaran, penelitian, dan pengembangan akademik siswa dan guru. Sebagai unit kerja pelaksana teknis (UPT) SMKN 2 Bangkalan, perpustakaan bertanggung jawab dalam memilah, menghimpun, mengolah, serta menyebarluaskan sumber informasi yang relevan kepada komunitas sekolah.
+
+    <p>
+    <h4>Perpustakaan SMKN 2 Bangkalan</h4>
+        Perpustakaan SMKN 2 Bangkalan berperan aktif dalam mendukung kegiatan akademik dan pengembangan diri siswa serta tenaga pendidik. Dengan lokasi yang mudah diakses dan koleksi yang beragam, perpustakaan memberikan layanan yang sesuai dengan kebutuhan belajar dan penelitian. Koleksi perpustakaan meliputi buku, materi referensi, jurnal, majalah, dan sumber informasi lainnya yang relevan dengan kurikulum sekolah.
+
+    <p>
+        Dalam lima tahun terakhir, perpustakaan SMKN 2 Bangkalan telah mengalami peningkatan yang signifikan dalam koleksi bahan bacaan serta layanan yang disediakan. Dukungan dan perhatian dari pihak sekolah memperkuat upaya pengembangan dan peningkatan kualitas layanan perpustakaan.
+
+    <p>
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+
+@endsection
